6f832d55586d0fecf8490c3fb7ac946542976b0a
mnngy <59257197+mnngy@users.noreply.github.com>
2021-09-17 16:54:47 +0900

    로그인, 회원가입 수정
    로그인, 회원가입 예외처리
    
    xml 수정

---
+++ app/src/main/java/com/example/localcommunity/Join.java
@@ -0,0 +1,102 @@
+package com.example.localcommunity;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Join extends AppCompatActivity {
+
+    EditText editJoinId, editJoinPwd, editJoinPwdConfirm, editJoinPhone;
+    private static final String TAG = "JOIN";
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+        // 액션바 설정
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        editJoinId = findViewById(R.id.join_edit_id);
+        editJoinPwd  = findViewById(R.id.join_edit_password);
+        editJoinPwdConfirm = findViewById(R.id.join_edit_password_confirm);
+        editJoinPhone = findViewById(R.id.join_edit_phone);
+
+        requestQueue = Volley.newRequestQueue(this);
+    }
+
+    public void join(View view) {
+        String url = "http://192.168.0.25:8080/member/join";
+
+        String id = editJoinId.getText().toString();
+        String pwd = editJoinPwd.getText().toString();
+        String pwdConfirm = editJoinPwdConfirm.getText().toString();
+        String phone = editJoinPhone.getText().toString();
+
+        if (id.length() == 0 || pwd.length() == 0 || pwdConfirm.length() == 0 || phone.length() == 0) {
+            Toast.makeText(getApplicationContext(), "입력하지 않은 값이 있습니다", Toast.LENGTH_SHORT).show();
+        } else {
+            if (pwd.equals(pwdConfirm)) {
+                Map<String, String> postParam= new HashMap<>();
+                postParam.put("memberId", editJoinId.getText().toString());
+                postParam.put("memberPassword", editJoinPwd.getText().toString());
+                postParam.put("memberPhone", editJoinPhone.getText().toString());
+
+                JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, new JSONObject(postParam), new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    String result = response.getString("result");
+                                    if (result.equals("success")) {
+                                        Toast.makeText(getApplicationContext(), "회원가입을 성공했습니다", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        finish();
+                                        startActivity(intent);
+                                    } else if (result.equals("duplicate")){
+                                        Toast.makeText(getApplicationContext(), "이미 존재하는 회원 아이디입니다", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    Log.d(TAG, e.toString());
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d(TAG, error.toString());
+                            }
+                        });
+                requestQueue.add(jsObjRequest);
+            } else {
+                Toast.makeText(getApplicationContext(), "비밀번호와 비밀번호 확인이 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void cancel(View view) {
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/localcommunity/Login.java
+++ app/src/main/java/com/example/localcommunity/Login.java
@@ -1,5 +1,6 @@
 package com.example.localcommunity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -25,7 +26,7 @@ import java.util.Map;
 public class Login extends AppCompatActivity {
 
     private static final String TAG = "LOGIN";
-    EditText editLogId, editLogPw;
+    EditText editLogId, editLogPwd;
     RequestQueue requestQueue;
 
     @Override
@@ -33,57 +34,53 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editLogId = findViewById(R.id.edit_log_id);
-        editLogPw = findViewById(R.id.edit_log_pw);
+        // 액션바 설정
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        editLogId = findViewById(R.id.login_edit_id);
+        editLogPwd = findViewById(R.id.login_edit_password);
+
+        requestQueue = Volley.newRequestQueue(this);
     }
 
     public void login(View view) {
         String url = "http://192.168.0.25:8080/member/login";
 
-        requestQueue = Volley.newRequestQueue(this);
-
-        Map<String, String> postParam= new HashMap<String, String>();
+        Map<String, String> postParam= new HashMap<>();
         postParam.put("memberId", editLogId.getText().toString());
-        postParam.put("memberPassword", editLogPw.getText().toString());
+        postParam.put("memberPassword", editLogPwd.getText().toString());
 
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
                 (Request.Method.POST, url, new JSONObject(postParam), new Response.Listener<JSONObject>() {
-
                     @Override
                     public void onResponse(JSONObject response) {
-
                         try {
                             String result = response.getString("result");
 
                             if (result.equals("success")) {
                                 Toast.makeText(getApplicationContext(), "로그인을 성공했습니다", Toast.LENGTH_SHORT).show();
-
                                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                startActivity(intent);
                                 finish();
+                                startActivity(intent);
                             } else {
                                 Toast.makeText(getApplicationContext(), "아아디 또는 비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
-                            e.printStackTrace();
+                            Log.d(TAG, e.toString());
                         }
-
-//                        String boardId = null;
-//                        try {
-//                            boardId = response.getString("boardId");
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // 자동으로 생성되는 메소드의 부분
                         Log.d(TAG, error.toString());
                     }
                 });
-
         requestQueue.add(jsObjRequest);
+    }
 
+    public void moveJoinForm(View view) {
+        Intent intent = new Intent(getApplicationContext(), Join.class);
+        startActivity(intent);
     }
 }
